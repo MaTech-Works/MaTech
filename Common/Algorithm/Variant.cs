210230c00c6d3fd9d792e1c68a8797d847d9ce9d
LuiCat <luicat@matech.work>
2022-12-10 23:59:34 +0800

    Refactor Variant to be using IBoxlessConvertible

--- Common/Algorithm/Variant.cs
+++ Common/Algorithm/Variant.cs
@@ -13,19 +13,20 @@ namespace MaTech.Common.Algorithm {
         None = 0, Bool, Int, Float, Double, Fraction, FractionSimple, String, Object
     }
 
-    /// <summary>
     /// A boolean, an integer, a float-point, a fraction, a string, an object, or nothing ("None" type).
     /// 
-    /// Numeral types can be casted between each other.
-    /// Every type can convert to string.
+    /// String and Object typed values are non-null; null values are of None type.
     /// 
-    /// String and Object values are non-null; a None value
-    /// The equality method compares the data strictly, including float-point values.
+    /// Numeral types can be converted between each other.
+    /// Every type can convert to a string by formatting or an object by boxing.
     ///
-    /// todo: implement IConvertible and conversion operators
-    /// </summary>
+    /// The equality method compares the type and value strictly, not tolerant to float-point errors.
+    ///
+    /// todo: getter for array type, also allowing fraction and array of int variant to exist simultaneously
+    /// todo: getter for table type
+    /// todo: do we infer VariantType from boxed objects?
     [JsonConverter(typeof(VariantJsonConverter))]
-    public readonly struct Variant : IEquatable<Variant> {
+    public readonly struct Variant : IEquatable<Variant>, IBoxlessConvertible, IFormattable {
         public VariantType Type { get; }
 
         private readonly FractionSimple f;
@@ -89,6 +90,7 @@ namespace MaTech.Common.Algorithm {
         public Variant(object value) {
             if (value == null) this = None;
             else {
+                // no type infer, object in object out
                 Type = VariantType.Object;
                 f = FractionSimple.invalid;
                 d = Double.NaN;
@@ -102,23 +104,16 @@ namespace MaTech.Common.Algorithm {
         public double Double => d;
         public Fraction Fraction => f;
         public FractionSimple FractionSimple => f;
-        public string String => ToString();
+        public string String => IsString ? (string)o : null; // no conversion by default
+        public object Object => IsObject ? o : null; // no conversion by default
 
-        public object Object {
-            get {
-                switch (Type) {
-                case VariantType.Bool: return Bool;
-                case VariantType.Int: return Int;
-                case VariantType.Float: return Float;
-                case VariantType.Double: return Double;
-                case VariantType.Fraction: return Fraction;
-                case VariantType.FractionSimple: return FractionSimple;
-                case VariantType.String:
-                case VariantType.Object: return o;
-                default: return null;
-                }
-            }
-        }
+        public override string ToString() => ToString(null, null);
+
+        public static Variant FromObject<T>(T value) => new Variant(value);
+        public T ToObject<T>() where T : class => ToObject() as T;
+        public T GetObject<T>() where T : class => Object as T;
+
+        public T To<T>(IFormatProvider provider = null) => BoxlessConvert.To<T>.FromBoxlessConvertible(this, provider);
 
         public static implicit operator Variant(bool value) => new Variant(value);
         public static implicit operator Variant(int value) => new Variant(value);
@@ -128,8 +123,43 @@ namespace MaTech.Common.Algorithm {
         public static implicit operator Variant(FractionSimple value) => new Variant(value);
         public static implicit operator Variant(string value) => new Variant(value);
 
-        public T GetObject<T>() where T : class => Object as T;
-        public static Variant FromObject<T>(T value) => new Variant(value);
+        private static Type cachedFractionType = typeof(Fraction);
+        private static Type cachedFractionSimpleType = typeof(FractionSimple);
+
+        bool IConvertible.ToBoolean(IFormatProvider provider) => Bool;
+
+        sbyte IConvertible.ToSByte(IFormatProvider provider) => (sbyte)Int;
+        short IConvertible.ToInt16(IFormatProvider provider) => (short)Int;
+        int IConvertible.ToInt32(IFormatProvider provider) => Int;
+        long IConvertible.ToInt64(IFormatProvider provider) => Int;
+
+        byte IConvertible.ToByte(IFormatProvider provider) => (byte)Int;
+        ushort IConvertible.ToUInt16(IFormatProvider provider) => (ushort)Int;
+        uint IConvertible.ToUInt32(IFormatProvider provider) => (uint)Int;
+        ulong IConvertible.ToUInt64(IFormatProvider provider) => (ulong)Int;
+
+        float IConvertible.ToSingle(IFormatProvider provider) => Float;
+        double IConvertible.ToDouble(IFormatProvider provider) => Double;
+        decimal IConvertible.ToDecimal(IFormatProvider provider) => (decimal)Double;
+
+        string IConvertible.ToString(IFormatProvider provider) => ToString(null, provider);
+
+        char IConvertible.ToChar(IFormatProvider provider) => throw new InvalidCastException("Variant: Conversion to char is undefined. Try a string or manually convert the charcode to a numeric type.");
+        DateTime IConvertible.ToDateTime(IFormatProvider provider) => throw new InvalidCastException("Variant: Conversion to DateTime is undefined. Try a string or a numeric timecode; choose wisely.");
+
+        object IConvertible.ToType(Type type, IFormatProvider provider) {
+            if (type == cachedFractionType) return Fraction;
+            if (type == cachedFractionSimpleType) return FractionSimple;
+            if (IsObject) Convert.ChangeType(o, type, provider);
+            throw new InvalidCastException($"Variant: Conversion to type {type} is undefined.");
+        }
+
+        T IBoxlessConvertible.ToType<T>(IFormatProvider provider) {
+            var type = typeof(T);
+            if (type == cachedFractionType) return BoxlessConvert.Identity<Fraction, T>(Fraction);
+            if (type == cachedFractionSimpleType) return BoxlessConvert.Identity<FractionSimple, T>(FractionSimple);
+            throw new InvalidCastException($"Variant: Boxless conversion to type {type} is undefined.");
+        }
 
         public bool IsNothing => Type == VariantType.None;
         public bool IsBoolean => Type == VariantType.Bool;
@@ -142,8 +172,21 @@ namespace MaTech.Common.Algorithm {
         public bool IsString => Type == VariantType.String;
         public bool IsStringEmpty => IsString && string.IsNullOrEmpty((string)o);
         public bool IsStringWhiteSpace => IsString && string.IsNullOrWhiteSpace((string)o);
+        public bool IsObject => Type == VariantType.Object;
 
-        public bool CanConvertTo(VariantType targetType) {
+        public TypeCode GetTypeCode() {
+            switch (Type) {
+            case VariantType.None: return TypeCode.Empty;
+            case VariantType.Bool: return TypeCode.Boolean;
+            case VariantType.Int: return TypeCode.Int32;
+            case VariantType.Float: return TypeCode.Single;
+            case VariantType.Double: return TypeCode.Double;
+            case VariantType.String: return TypeCode.String;
+            default: return TypeCode.Object; // Fraction, FractionSimple, Object
+            }
+        }
+
+        public bool HasValueOfType(VariantType targetType, bool allowConversion = false) {
             if (Type == targetType) return true;
             if (Type == VariantType.None) return true; // false, 0, 0.0, "<None Variant>", null
             switch (targetType) {
@@ -154,24 +197,36 @@ namespace MaTech.Common.Algorithm {
             case VariantType.Double:
             case VariantType.Fraction:
             case VariantType.FractionSimple: return IsNumeralOrBoolean;
-            case VariantType.String: return true;
-            case VariantType.Object: return true; // null when not a string or object
+            case VariantType.String: return allowConversion;
+            case VariantType.Object: return allowConversion;
             default: return false;
             }
         }
 
-        public override string ToString() {
+        public string ToString(string format, IFormatProvider formatProvider) {
             switch (Type) {
             case VariantType.None: return "<None Variant>";
-            case VariantType.Bool: return Bool ? "True" : "False";
-            case VariantType.Int: return Int.ToString(CultureInfo.InvariantCulture);
-            case VariantType.Float: return Float.ToString(CultureInfo.InvariantCulture);
-            case VariantType.Double: return Double.ToString(CultureInfo.InvariantCulture);
+            case VariantType.Bool: return Bool.ToString(formatProvider);
+            case VariantType.Int: return Int.ToString(format, formatProvider);
+            case VariantType.Float: return Float.ToString(format, formatProvider);
+            case VariantType.Double: return Double.ToString(format, formatProvider);
             case VariantType.Fraction: return Fraction.ToString();
             case VariantType.FractionSimple: return FractionSimple.ToString();
-            case VariantType.String: return (string)o;
-            case VariantType.Object: return o.ToString();
-            default: return "<Unknown Variant>";
+            case VariantType.String: return String;
+            case VariantType.Object: return Convert.ToString(o, formatProvider);
+            default: return "<Undefined Variant>";
+            }
+        }
+
+        public object ToObject() {
+            switch (Type) {
+            case VariantType.Bool: return Bool;
+            case VariantType.Int: return Int;
+            case VariantType.Float: return Float;
+            case VariantType.Double: return Double;
+            case VariantType.Fraction: return Fraction;
+            case VariantType.FractionSimple: return FractionSimple;
+            default: return o; // String and Object
             }
         }
 
@@ -195,38 +250,19 @@ namespace MaTech.Common.Algorithm {
             return obj is Variant other && Equals(other);
         }
 
-        public override int GetHashCode() {
-            unchecked {
-                int hashCode;
-                switch (Type) {
-                case VariantType.None:
-                    hashCode = 0;
-                    break;
-                case VariantType.Bool:
-                    hashCode = Bool.GetHashCode();
-                    break;
-                case VariantType.Int:
-                    hashCode = Int.GetHashCode();
-                    break;
-                case VariantType.Float:
-                    hashCode = Float.GetHashCode();
-                    break;
-                case VariantType.Double:
-                    hashCode = Double.GetHashCode();
-                    break;
-                case VariantType.Fraction:
-                    hashCode = Fraction.GetHashCode();
-                    break;
-                case VariantType.FractionSimple:
-                    hashCode = FractionSimple.GetHashCode();
-                    break;
-                case VariantType.String:
-                case VariantType.Object:
-                    hashCode = o.GetHashCode();
-                    break;
-                default: return 0;
-                }
-                return  HashCode.Combine(hashCode, Type.GetHashCode());
+        public override int GetHashCode() => GetHashCode(true);
+        public int GetHashCode(bool withVariantType) {
+            if (withVariantType) return HashCode.Combine(GetHashCode(false), Type.GetHashCode());
+            switch (Type) {
+            case VariantType.Bool: return Bool.GetHashCode();
+            case VariantType.Int: return Int.GetHashCode();
+            case VariantType.Float: return Float.GetHashCode();
+            case VariantType.Double: return Double.GetHashCode();
+            case VariantType.Fraction: return Fraction.GetHashCode();
+            case VariantType.FractionSimple: return FractionSimple.GetHashCode();
+            case VariantType.String:
+            case VariantType.Object: return o.GetHashCode();
+            default: return 0; // None
             }
         }
 
@@ -245,73 +281,37 @@ namespace MaTech.Common.Algorithm {
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
             var variant = (Variant)value;
             switch (variant.Type) {
-            case VariantType.None:
-                writer.WriteNull();
-                break;
-            case VariantType.Bool:
-                writer.WriteValue(variant.Bool);
-                break;
-            case VariantType.Int:
-                writer.WriteValue(variant.Int);
-                break;
-            case VariantType.Float:
-                writer.WriteValue(variant.Float);
-                break;
-            case VariantType.Double:
-                writer.WriteValue(variant.Double);
-                break;
-            case VariantType.Fraction:
-                serializer.Serialize(writer, variant.Fraction);
-                break;
-            case VariantType.FractionSimple:
-                serializer.Serialize(writer, variant.FractionSimple);
-                break;
-            case VariantType.String:
-                writer.WriteValue(variant.String);
-                break;
-            case VariantType.Object:
-                writer.WriteValue(variant.Object);
-                break;
-            default:
-                writer.WriteNull();
-                break;
+            case VariantType.None: writer.WriteNull(); break;
+            case VariantType.Bool: writer.WriteValue(variant.Bool); break;
+            case VariantType.Int: writer.WriteValue(variant.Int); break;
+            case VariantType.Float: writer.WriteValue(variant.Float); break;
+            case VariantType.Double: writer.WriteValue(variant.Double); break;
+            case VariantType.Fraction: serializer.Serialize(writer, variant.Fraction); break;
+            case VariantType.FractionSimple: serializer.Serialize(writer, variant.FractionSimple); break;
+            case VariantType.String: writer.WriteValue(variant.String); break;
+            case VariantType.Object: writer.WriteValue(variant.Object); break;
+            default: writer.WriteNull(); break;
             }
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
             var variant = (Variant?)existingValue ?? Variant.None;
             switch (reader.TokenType) {
-            case JsonToken.Boolean:
-                variant = serializer.Deserialize<bool>(reader);
-                break;
-            case JsonToken.Integer:
-                variant = serializer.Deserialize<int>(reader);
-                break;
-            case JsonToken.Float:
-                variant = serializer.Deserialize<double>(reader);
-                break; // unfortunately we cannot distinguish between f32 & f64
-            case JsonToken.String:
-                variant = serializer.Deserialize<string>(reader);
-                break;
+            case JsonToken.Boolean: variant = serializer.Deserialize<bool>(reader); break;
+            case JsonToken.Integer: variant = serializer.Deserialize<int>(reader); break;
+            case JsonToken.Float: variant = serializer.Deserialize<double>(reader); break; // unfortunately we cannot distinguish between f32 & f64
+            case JsonToken.String: variant = serializer.Deserialize<string>(reader); break;
 
             case JsonToken.Raw:
             case JsonToken.Date:
-            case JsonToken.Bytes:
-                variant = Variant.FromObject(serializer.Deserialize(reader));
-                break;
+            case JsonToken.Bytes: variant = Variant.FromObject(serializer.Deserialize(reader)); break;
 
             case JsonToken.StartArray:
                 var arr = FractionJsonConverter.ReadIntArrayForFraction(reader);
                 switch (arr.Count) {
-                case 2:
-                    variant = new FractionSimple(arr[0], arr[1]);
-                    break;
-                case 3:
-                    variant = new Fraction(arr[0], arr[1], arr[2]);
-                    break;
-                default:
-                    variant = Variant.None;
-                    break;
+                case 2: variant = new FractionSimple(arr[0], arr[1]); break;
+                case 3: variant = new Fraction(arr[0], arr[1], arr[2]); break;
+                default: variant = Variant.None; break;
                 }
                 break;
             }
