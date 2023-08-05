6226ad6555ebcacb757129e5feb2f8e2356a3805
LuiCat <luicat@matech.work>
2023-08-06 02:31:55 +0800

    Preserve MaGameplay assembly and enums

---
+++ Gameplay/Preserve.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2023, LuiCat (as MaTech)
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using MaTech.Common.Algorithm;
+using MaTech.Gameplay.Data;
+using MaTech.Gameplay.Scoring;
+using UnityEngine.Scripting;
+
+[assembly: AlwaysLinkAssembly, Preserve]
+
+namespace MaTech.Gameplay {
+    internal static class PreserveStub {
+        [Preserve]
+        public static void PreserveStub_DoNotCall() {
+            BoxlessConvert.PreserveForEnum<EffectType>();
+            BoxlessConvert.PreserveForEnum<ScoreType>();
+            BoxlessConvert.PreserveForEnum<HitResult>();
+            BoxlessConvert.PreserveForEnum<JudgeLogicBase.NoteHitAction>();
+            BoxlessConvert.PreserveForEnum<JudgeLogicBase.EmptyHitAction>();
+            BoxlessConvert.PreserveForEnum<ChartPlayer.ObjectType>();
+        }
+    }
+}
