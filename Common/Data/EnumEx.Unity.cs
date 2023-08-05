929d69dc522e51ad568e6826e8f562bed1475b1d
LuiCat <luicat@matech.work>
2023-08-06 02:23:34 +0800

    Add shortcut attribute to initialize EnumEx values at correct timing

---
+++ Common/Data/EnumEx.Unity.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2023, LuiCat (as MaTech)
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using UnityEditor;
+
+namespace MaTech.Common.Data {
+    public class InitializeEnumExMethodAttribute
+#if UNITY_EDITOR
+        : InitializeOnLoadMethodAttribute 
+#else
+        : RuntimeInitializeOnLoadMethodAttribute
+#endif
+    {
+#if !UNITY_EDITOR
+        public InitializeEnumExMethodAttribute() : base(RuntimeInitializeLoadType.SubsystemRegistration) { }
+#endif
+    }
+}
