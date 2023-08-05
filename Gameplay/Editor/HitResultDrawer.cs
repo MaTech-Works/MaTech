a912c3f6dfade59b8b16f2ffbc98d05d4b216759
LuiCat <luicat@matech.work>
2023-08-06 02:26:41 +0800

    Add HitResultDrawer as editor codes into MaGameplay

---
+++ Gameplay/Editor/HitResultDrawer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2023, LuiCat (as MaTech)
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using MaTech.Gameplay.Scoring;
+using UnityEditor;
+using UnityEngine;
+
+namespace MaTech.Gameplay.Editor {
+    #if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(HitResult))]
+    public class HitResultDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginChangeCheck();
+            var maskValue = EditorGUI.MaskField(position, label, property.intValue, HitResultEditorNames.Value);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.intValue = maskValue;
+            }
+        }
+    }
+    #endif
+}
