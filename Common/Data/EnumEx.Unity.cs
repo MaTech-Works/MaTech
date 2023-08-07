943c0c90c2ce72ca1e242114cb52e3fcd1409b03
LuiCat <luicat@matech.work>
2023-08-08 02:42:22 +0800

    Fix using for EnumEx attribute (wrongly removed by batch cleanup)

--- Common/Data/EnumEx.Unity.cs
+++ Common/Data/EnumEx.Unity.cs
@@ -4,7 +4,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#if UNITY_EDITOR
 using UnityEditor;
+#else
+using UnityEngine;
+#endif
 
 namespace MaTech.Common.Data {
     public class InitializeEnumExMethodAttribute
