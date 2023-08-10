c38c58f4088bddcc78752e081e1a83a95532bbb2
LuiCat <luicat@matech.work>
2023-08-10 10:25:54 +0800

    MetaTable methods with string keys

--- Common/Data/MetaTable.cs
+++ Common/Data/MetaTable.cs
@@ -25,10 +25,18 @@ namespace MaTech.Common.Data {
         public T GetOrSet<T>(EnumEx<TEnum> key, in T value) => ValueDictOf<T>(key, value).GetOrSet(key, value);
         public T Set<T>(EnumEx<TEnum> key, in T value) => ValueDictOf<T>(key, value).Set(key, value);
         
+        public bool Has<T>(string keyName) => Has<T>(new EnumEx<TEnum>(keyName)); 
+        public bool Remove<T>(string keyName) => Remove<T>(new EnumEx<TEnum>(keyName)); 
+        public T Get<T>(string keyName) => Get<T>(new EnumEx<TEnum>(keyName)); 
+        public T? GetNullable<T>(string keyName) where T : struct => GetNullable<T>(new EnumEx<TEnum>(keyName)); 
+        public T GetOrSet<T>(string keyName, in T value) => GetOrSet<T>(new EnumEx<TEnum>(keyName), value); 
+        public T Set<T>(string keyName, in T value) => Set<T>(new EnumEx<TEnum>(keyName), value); 
+        
         public int Collect<T>(ICollection<T> outList) => ValueDictOf<T>().Collect(outList);
 
         public Selector<Root, TEnum> Select(TEnum key) => CreateRootSelector(key);
         public Selector<Root, TEnum> Select(EnumEx<TEnum> key) => CreateRootSelector(key.Value);
+        public Selector<Root, TEnum> Select(string keyName) => CreateRootSelector(new EnumEx<TEnum>(keyName).Value);
         
         public partial struct Selector<TContext, TEnum0> {
             public bool IsValid() => context.GetTable<TEnum0>() != null;
@@ -51,6 +59,7 @@ namespace MaTech.Common.Data {
             
             public Selector<Selector<TContext, TEnum0>, TEnum1> Select<TEnum1>(TEnum1 key1) where TEnum1 : unmanaged, Enum, IConvertible => CreateNestedSelector(this, key1);
             public Selector<Selector<TContext, TEnum0>, TEnum1> Select<TEnum1>(EnumEx<TEnum1> key1) where TEnum1 : unmanaged, Enum, IConvertible => CreateNestedSelector(this, key1.Value);
+            public Selector<Selector<TContext, TEnum0>, TEnum1> Select<TEnum1>(string keyName1) where TEnum1 : unmanaged, Enum, IConvertible => CreateNestedSelector(this, new EnumEx<TEnum1>(keyName1).Value);
         }
     }
 }
