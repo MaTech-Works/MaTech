4f799422bcda64559c9988ffeee613d7663566f1
LuiCat <luicat@matech.work>
2023-08-12 02:58:53 +0800

    HitEventBehavior, a default and example usage of HitEvent

---
+++ Gameplay/Utils/HitEventBehavior.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2023, LuiCat (as MaTech)
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using MaTech.Gameplay.Display;
+using MaTech.Gameplay.Scoring;
+using MaTech.Gameplay.Time;
+
+namespace MaTech.Gameplay.Utils {
+    public class HitEventBehavior : PlayBehavior, PlayBehavior.INoteHitResult {
+        public HitEvent[] hitEvents;
+
+        public void OnHitNote(ChartPlayer.NoteCarrier carrier, JudgeLogicBase.NoteHitAction action, TimeUnit judgeTime, HitResult result) {
+            foreach (var hitEvent in hitEvents) {
+                hitEvent.InvokeIfMatch(action, result);
+            }
+        }
+        
+        public void OnHitEmpty(JudgeLogicBase.EmptyHitAction action, TimeUnit judgeTime) {}
+    }
+}
