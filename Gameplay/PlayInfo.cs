61a79bff0e99d0208bb4bd797334473bed5ee7e8
LuiCat <luicat@matech.work>
2023-08-22 03:21:09 +0800

    PlayInfo Metadata

--- Gameplay/PlayInfo.cs
+++ Gameplay/PlayInfo.cs
@@ -4,6 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+using MaTech.Common.Data;
 using MaTech.Gameplay.Data;
 using MaTech.Gameplay.Time;
 
@@ -11,6 +12,17 @@ namespace MaTech.Gameplay {
     // TODO: 考虑一下PlayInfo结构是否从继承改为组合
     public interface IPlayInfo {
         Chart Chart { get; }
+
+        public enum MetaType {
+            Summary, Comment,
+            Title, Artist, Author, Difficulty, Level, Style,
+            TitleRomanized, ArtistRomanized, AuthorRomanized,
+            StartTime, EndTime, Length,
+            Tempo, MinTempo, MaxTempo, MeanTempo,
+            NoteCount, AccuracyDenominator, MaxCombo, MaxScore
+        }
+        
+        MetaTable<MetaType> Metadata { get; }
         
         // TODO: 根据ChartPlayer需要内部处理的信息，多加几个默认的时间定义
         TimeUnit? TrackStartTime { get; }
