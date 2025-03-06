c59dd1730781deec16890e9d2a0a53ab7a204a84
LuiCat <luicat@matech.work>
2025-03-06 11:21:02 +0800

    FuncRAII

---
+++ Common/Algorithm/FuncRAII.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2025, LuiCat (as MaTech)
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System;
+
+namespace MaTech.Common.Algorithm {
+	public static class FuncRAII {
+        public static FuncRAII<T> Lock<T>(this T self, Action<T> actionLock, Action<T> actionUnlock) => new(self, actionLock, actionUnlock);
+    }
+
+    public readonly struct FuncRAII<T> : IDisposable {
+		private readonly Action<T> actionUnlock;
+		private readonly T self;
+
+        public FuncRAII(in T self, Action<T> actionLock, Action<T> actionUnlock) {
+            this.actionUnlock = actionUnlock;
+            this.self = self;
+            actionLock?.Invoke(self);
+        }
+
+        public void Dispose() {
+            actionUnlock?.Invoke(self);
+        }
+    }
+}
